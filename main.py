cbefab9efcf63a1882763120803fcc3df37841ac
Evandir B Viegas <evandir@kwinfo.com.br>
2024-08-28 11:59:54 -0300

    Adicionando endpoint para dispositivos

--- main.py
+++ main.py
@@ -9,16 +9,14 @@ import random
 app = Flask(__name__)
 
 # Ler o JSON de um arquivo externo
-with open('dispositivos.json', 'r') as file:
-    dispositivos = json.load(file)
+def load_data():
+    with open('dispositivos.json') as f:
+        return json.load(f)
 
 
-# Função auxiliar para buscar informações por nome
-def buscar_por_nome(nome):
-    for i in range(len(dispositivos["nome"])):
-        if dispositivos["nome"][str(i)] == nome:
-            return i  # Retorna o índice onde o nome foi encontrado
-    return None
+@app.route('/dispositivos', methods=['GET'])
+def get_dispositivos():
+    return jsonify(load_data())
 
 
 # construir as funcionalidades
@@ -29,29 +27,18 @@ def homepage():
 
 @app.route('/lista_dispositivos_cadastrados')
 def lista_dispositivos_cadastrados():
-    # Criar um array combinando os valores do JSON
-    resultado = [{
-        "nome": dispositivos["nome"][str(i)],
-        "status": dispositivos["status"][str(i)]
-    } for i in range(len(dispositivos["nome"]))]
+    data = load_data()
+    return render_template('dispositivos.html', data=data)
 
-    # Retorna o array como JSON
-    return render_template('dispositivos.html', dispositivos=resultado)
-
-
-@app.route('/busca_status_dispositivo', methods=['GET'])
-def get_dados():
-    nome = request.args.get('nome')  # Obtém o parâmetro 'nome' da URL
-
-    if nome:
-        indice = buscar_por_nome(nome)
-        if indice is not None:
-            resultado = {"nome": nome, "status": dispositivos["status"][str(indice)]}
-            return jsonify(resultado)
-        else:
-            return jsonify({"error": "Nome não encontrado"}), 404
-    else:
-        return jsonify({"error": "Parâmetro 'nome' é necessário"}), 400
+@app.route('/busca_status_dispositivo/<int:codigo>', methods=['GET'])
+def get_status(codigo):
+    data = load_data()
+    # Encontrar o status com base no código fornecido
+    for item in data:
+        if item['codigo'] == codigo:
+            return jsonify({"codigo": codigo, "nome": item['nome'], "status": item['status']})
+    # Se o código não for encontrado
+    return jsonify({"error": "Código não encontrado"}), 404
 
 
 @app.route('/altera_status_dispositivo', methods=['PUT'])
